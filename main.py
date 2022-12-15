479c4ed1e50753250679da17e379d813cc472e5c
Бояков Николай <kolia.boyackov@yandex.ru>
2022-12-15 22:46:35 +0300

    Add Docstring

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Атака."""
     if char_class == 'warrior':
         return (
             f'{char_name} нанёс урон противнику равный '
@@ -20,6 +22,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Защита."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -29,6 +32,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Умения."""
     if char_class == 'warrior':
         return (
             f'{char_name} применил специальное умение'
@@ -41,6 +45,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Начало игры."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -66,6 +71,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Выбор класса."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -85,14 +91,15 @@ def choice_char_class() -> str:
                 'Лекарь — могущественный заклинатель.'
                 'Черпает силы из природы, веры и духов.')
         approve_choice: str = input(
-            'Нажми (Y), чтобы подтвердить выбор,'
-            'или любую другую кнопку,'
-            'чтобы выбрать другого персонажа '
+            'Нажми (Y), чтобы подтвердить выбор,',
+            'или любую другую кнопку,',
+            'чтобы выбрать другого персонажа ',
         ).lower()
     return char_class
 
 
-def main() -> str:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
